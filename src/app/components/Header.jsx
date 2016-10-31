15ad54a190879f4c4f83a5760db07040bb7bbe1f
Harry Birch <harry@sprint-education.co.uk>
2016-10-31 09:46:52 +0000

    Fix Header search input

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -19,19 +19,16 @@ const Header = React.createClass({
                 <div className='col-lg-1 col-sm-2 col-xs-2'>
                   <Link to='/' className='navbar-brand'><img src='/images/logo.svg' alt='Project name' className='logo' /></Link>
                 </div>
-                <div className='visible-xs visible-sm clearfix'></div>
-                <div className='col-lg-6 col-sm-8 col-xs-12'>
+                <div className='col-lg-6 col-sm-8 col-xs-10'>
                   <form onSubmit={this.props.handleSearchSubmitEvent}>
                     <div className='topsearch'>
                       <div className='input-group'>
                         <span className='input-group-addon' id='sizing-addon2'><i className='fa fa-search' /></span>
                         <input value={this.props.searchTerm} type='text' className='form-control' placeholder='Search for a movie...' onChange={this.props.handleSearchTermEvent} autoFocus='' />
-                        <div className='input-group-btn' style={{width: '100%'}} />
                       </div>
                     </div>
                   </form>
                 </div>
-                <div className='visible-xs clearfix'></div>
               </div>
             </div>
           </div>
